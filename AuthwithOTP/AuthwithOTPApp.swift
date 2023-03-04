3e05bc427379ce1425c6f02edd12624810e83a3d
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-03-04 18:26:40 +0000

    Initial Commit

---
+++ AuthwithOTP/AuthwithOTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthwithOTPApp.swift
+//  AuthwithOTP
+//
+//  Created by Abdoulaye Aliou SALL on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AuthwithOTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthwithOTP/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AuthwithOTP
+//
+//  Created by Abdoulaye Aliou SALL on 04/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

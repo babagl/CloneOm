1f48e696070357f7918146b816f11891049a8da6
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-03-27 19:05:22 +0000

    message

--- AuthwithOTP/Views/ProfileView.swift
+++ AuthwithOTP/Views/ProfileView.swift
@@ -58,19 +58,37 @@ struct ProfileView_Previews: PreviewProvider {
 }
 
 struct ProfileSubview : View {
+    @State var shouldShowImagePicker = false
+    @State var image :UIImage?
     var body: some View{
         
         VStack{
             ZStack {
-                Button(action: {}){
+                Button(action: {
+                    shouldShowImagePicker.toggle()
+                }){
                     ZStack {
-                        Image("profil")
+                        if let image = self.image{
+                            Image(uiImage: image)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 130, height: 130)
+                                .cornerRadius(80)
+                            //.padding(.top, -80)
+                            Image(systemName: "plus.app.fill")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color("OrangeM"))
+                                .padding(.leading,60)
+                            .padding(.top,80)
+                            
+                        }else{
+                            Image("profil")
                             .resizable()
                             .clipShape(Circle())
                             .aspectRatio(contentMode: .fill)
-                        //.border(Color.gray , width: 2)
-                            .overlay(RoundedRectangle(cornerRadius: 50).stroke(Color.black.opacity(0.3), lineWidth: 3)
-                            )
+                        
+                            .overlay(RoundedRectangle(cornerRadius: 50).stroke(Color.black.opacity(0.3), lineWidth: 3))
                             .frame(width: 100, height: 100)
                         //.padding(.top, -80)
                         Image(systemName: "plus.app.fill")
@@ -78,7 +96,8 @@ struct ProfileSubview : View {
                             .fontWeight(.bold)
                             .foregroundColor(Color("OrangeM"))
                             .padding(.leading,60)
-                            .padding(.top,80)
+                        .padding(.top,80)
+                        }
                     }
                     
                 }
@@ -97,6 +116,49 @@ struct ProfileSubview : View {
             .padding()
         }
         .padding(.top)
+        .fullScreenCover(isPresented: $shouldShowImagePicker, onDismiss: nil){
+            ImagePicker(image: $image)
+        }
 
         }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+ 
+    @Binding var image: UIImage?
+ 
+    private let controller = UIImagePickerController()
+ 
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+ 
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+ 
+        let parent: ImagePicker
+ 
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+ 
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            parent.image = info[.originalImage] as? UIImage
+            picker.dismiss(animated: true)
+        }
+ 
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true)
+        }
+ 
+    }
+ 
+    func makeUIViewController(context: Context) -> some UIViewController {
+        controller.delegate = context.coordinator
+        return controller
+    }
+ 
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+ 
+    }
+ 
 }
